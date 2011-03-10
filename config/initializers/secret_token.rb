b16975202c9ab5d86f129a0e0a117c17579cb3ac
asoltys <asoltys@87784157-645b-4f6f-817d-e872428646e8>
2011-03-10 03:20:00 +0000

    add rails 3.0.5 application skeleton
    git-svn-id: https://svn.ircan.gc.ca/svn/wet-boew-ruby-rails@2 87784157-645b-4f6f-817d-e872428646e8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WetBoewRubyRails::Application.config.secret_token = '04170403bfe5900e86894e84f22a5d952155c99b9f8ab6fb1bca72b88e13129cbcdf8d61963cd5a7994bc0eb9bd470b5fa0b295f53268186ec50f0a18df6e2a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WetBoewRubyRails::Application.config.session_store :cookie_store, :key => '_wet-boew-ruby-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WetBoewRubyRails::Application.config.session_store :active_record_store
