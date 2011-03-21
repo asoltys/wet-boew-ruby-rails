d14f3457eb85729f1c61774b669b444c16b63401
asoltys <asoltys@87784157-645b-4f6f-817d-e872428646e8>
2011-03-21 04:04:11 +0000

    get splash page working with wp-pa layout
    git-svn-id: https://svn.ircan.gc.ca/svn/wet-boew-ruby-rails@21 87784157-645b-4f6f-817d-e872428646e8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+    render :layout => 'wp-pa'
+  end
   
   def home
-    render :layout => "3col"
+    render :layout => '3col'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 WetBoewRubyRails::Application.routes.draw do
-  root :to => "application#home"
+  root :to => "application#index"
+  match "/:action", :to => "application"
 end
