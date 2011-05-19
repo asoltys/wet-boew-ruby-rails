4480f0fe18efbcea020ece3d0021cf91fcdb3e85
asoltys <asoltys@87784157-645b-4f6f-817d-e872428646e8>
2011-05-19 04:57:13 +0000

    basic language switching (refs #2153)
    git-svn-id: https://svn.ircan.gc.ca/svn/wet-boew-ruby-rails@31 87784157-645b-4f6f-817d-e872428646e8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   def index
     render :layout => 'wp-pa'
@@ -16,4 +17,29 @@ class ApplicationController < ActionController::Base
   def threecol
     render :layout => '3col'
   end
+
+  def parse_lang_parameter(url)
+    return url unless params[:lang]
+
+    if I18n.available_locales.include?(params[:lang].to_sym)
+      session[:language] = params[:lang]
+    end
+
+    url = url.sub(/\?.*/, '')
+
+    query_string = request.query_string.sub(/&lang(\=[^&]*)?(?=&|$)|^lang(\=[^&]*)?(&|$)/, '')
+    url = url + "?#{query_string}" unless query_string.empty?
+    return url
+  end
+
+  def set_locale
+    url = parse_lang_parameter(request.url) 
+      
+    if request.get? && url != request.url
+      head :moved_permanently, :location => url 
+    end
+
+    I18n.locale = session[:language]
+  end
+
 end
